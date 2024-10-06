235ef9b4861b7a742712c08be714e652a355bfd7
Akasha005 <168619909+Akasha005@users.noreply.github.com>
2024-10-06 13:13:34 +0530

    Update and rename basicoperation.bash to basicoperation.js

--- Semester 4/MongoDB/basicoperation.bash
+++
@@ -1,8 +0,0 @@
-# Question: Illustration of Where Clause, AND, OR operations
-# File: employee_data.json
-db.collection.find({
-    $or: [
-        { age: { $gt: 25 } },   # age greater than 25
-        { status: "active" }    # or status is 'active'
-    ]
-})
