c6ae74576882fc1748b44be604fef4ba7df946ad
Akasha005 <168619909+Akasha005@users.noreply.github.com>
2024-10-06 13:15:32 +0530

    Update and rename projection operators.bash to projection operators.js

--- Semester 4/MongoDB/projection operators.bash
+++
@@ -1,8 +0,0 @@
-# Question: Demonstrate projection operators ($, $elemMatch, $slice)
-# File: employee_data.json
-
-# $elemMatch to project the first matching sub-document
-db.collection.find({ "grades": { $elemMatch: { score: { $gt: 90 } } } })
-
-# $slice to limit the number of array elements returned
-db.collection.find({}, { grades: { $slice: 2 } })
